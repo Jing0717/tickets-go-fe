c8444c0e803dfaf8069d145e3db167e028f291cf
Dotzooo <projectnba77@gmail.com>
2024-05-28 12:09:43 +0800

    feat: react-toastify plugin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,27 @@
 // pages/_app.tsx
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { Provider } from 'react-redux';
-import { store } from '../store/store';
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
+import { store } from '../store/store'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <ToastContainer
+        position='top-right'
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='light'
+      />
       <Component {...pageProps} />
     </Provider>
-  );
+  )
 }
