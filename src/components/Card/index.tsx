4054ff81473c6930cb20622803313a1574eb632e
Jing <bleach81618@gmail.com>
2024-07-02 16:31:09 +0800

    feat: create Card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark } from '@fortawesome/free-regular-svg-icons'
+import styles from './Card.module.css'
+
+const Card = ({ img, title }) => {
+  return (
+    <div
+      className={` w-[400px] mt-10 bg-white rounded-lg shadow-md border-2 border-dashed border-gray-300 relative flex-shrink-0 ${styles.card}`}
+    >
+      <button
+        type='button'
+        className='absolute top-2 right-2 bg-white bg-opacity-70 w-[44px] h-[44px] flex justify-center items-center flex-shrink-0 z-10'
+      >
+        <FontAwesomeIcon icon={faBookmark} className='w-[21px] h-[27px] cursor-pointer bg-transparent' />
+      </button>
+      <div className='relative w-full h-64 mb-4'>
+        <Image src={img} layout='fill' objectFit='cover' alt='Event Image' className='' />
+      </div>
+      <div className='p-4'>
+        <button type='button' className='bg-[#DC4B4B] py-2 px-4 text-white font-semibold rounded w-full mb-4'>
+          立刻訂購
+        </button>
+        <div className=''>
+          <h2 className='text-lg font-semibold mb-2'>{title}</h2>
+          <div className='text-gray-500 mb-2'>
+            <div>
+              <span>2025/02/30</span>
+              <span className='mx-2'>17:30</span>
+            </div>
+            <div>TAIWAN</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,75 +1,64 @@
-// import { useSelector } from 'react-redux';
-// import { useRouter } from 'next/router';
-// import { useGetUsersQuery } from '../store/authApi';
-// import { clearToken } from '@/store/slices/authSlice';
-// import { useDispatch } from 'react-redux';
-import Layout from '@/components/Layout';
-import Image from 'next/image';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBookmark } from '@fortawesome/free-solid-svg-icons';
-
-// interface User {
-//   id: number;
-//   name: string;
-// }
+import Layout from '@/components/Layout'
+import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark as solidfaBookmark } from '@fortawesome/free-solid-svg-icons'
+import Card from '@/components/Card'
 
 export default function Home() {
-  // const { data, isLoading } = useGetUsersQuery();
-  // const authToken = useSelector((state: any) => state.auth.token);
-  // const dispatch = useDispatch();
-  // const router = useRouter();
-
-  // const handleLoginClick = () => {
-  //   router.push('/login');
-  // };
-
-  // const handleLogout = async () => {
-  //   try {
-  //     dispatch(clearToken());
-  //     router.push('/login');
-  //   } catch (error) {
-  //     console.error('Logout failed:', error);
-  //   }
-  // };
-
-  // if (isLoading) return <div>Loading...</div>;
-
   return (
     <Layout>
-      <div className="relative pb-8 md:pb-0 w-full min-h-[625px] h-auto md:min-h-[807px] flex items-end md:items-center overflow-hidden">
-        <div className="absolute inset-0 bg-cover bg-center opacity-20 blur-sm" style={{ backgroundImage: `url('/hero.jpeg')` }}></div>
-        <div className="absolute inset-0" style={{ background: 'linear-gradient(180deg, #FFFBF5 0%, rgba(255, 251, 245, 0) 100%)' }}></div>
+      <div className='relative pb-8 md:pb-0 w-full min-h-[625px] h-auto md:min-h-[807px] flex items-end md:items-center overflow-hidden'>
+        <div
+          className='absolute inset-0 bg-cover bg-center opacity-20 blur-sm'
+          style={{ backgroundImage: `url('/hero.jpeg')` }}
+        ></div>
+        <div
+          className='absolute inset-0'
+          style={{ background: 'linear-gradient(180deg, #FFFBF5 0%, rgba(255, 251, 245, 0) 100%)' }}
+        ></div>
 
-        <div className="container mx-auto flex flex-col-reverse md:flex-row relative z-10 ">
-          <div className="w-full pt-6 mt-6 md:mt-0 md:w-1/2 md:pr-4">
-            <div className="flex justify-between">
-              <p className="text-gray-01 text-2xl leading-7 font-bold mb-4 mr-2 md:text-6xl md:leading-tight">SUPER JUNIOR-L.S.S.THE SHOW ： Th3ee Guys in TAIPEI</p>
-              <button type="button" className='bg-white bg-opacity-70 w-[44px] h-[44px] md:w-14 md:h-14 flex justify-center items-center flex-shrink-0'>
-                <FontAwesomeIcon
-                  icon={faBookmark}
-                  className="w-[21px] h-[27px] text-[#DC4B4B] cursor-pointer"
-                />
+        <div className='container mx-auto flex flex-col-reverse md:flex-row relative z-10 '>
+          <div className='w-full pt-6 mt-6 md:mt-0 md:w-1/2 md:pr-4'>
+            <div className='flex justify-between'>
+              <p className='text-gray-01 text-2xl leading-7 font-bold mb-4 mr-2 md:text-6xl md:leading-tight'>
+                SUPER JUNIOR-L.S.S.THE SHOW ： Th3ee Guys in TAIPEI
+              </p>
+              <button
+                type='button'
+                className='bg-white bg-opacity-70 w-[44px] h-[44px] md:w-14 md:h-14 flex justify-center items-center flex-shrink-0'
+              >
+                <FontAwesomeIcon icon={solidfaBookmark} className='w-[21px] h-[27px] text-[#DC4B4B] cursor-pointer' />
               </button>
             </div>
-            <p className="text-base md:text-2xl md:leading-9">The Th3ee Guys are coming to Taipei with "style" Are you guys ready to 'Suit Up' for an awesome night??</p>
-            <div className="flex font-medium justify-between mt-6 md:mt-20">
-              <button type="button" className='bg-[#DC4B4B] py-4 px-16 leading-5 text-white mr-6'>立刻訂購</button>
-              <button type="button" className='py-4 px-16 leading-5 border-2 border-black'>更多資訊</button>
+            <p className='text-base md:text-2xl md:leading-9'>
+              The Th3ee Guys are coming to Taipei with "style" Are you guys ready to 'Suit Up' for an awesome night??
+            </p>
+            <div className='flex font-medium justify-between mt-6 md:mt-20'>
+              <button type='button' className='bg-[#DC4B4B] py-4 px-16 leading-5 text-white mr-6'>
+                立刻訂購
+              </button>
+              <button type='button' className='py-4 px-16 leading-5 border-2 border-black'>
+                更多資訊
+              </button>
             </div>
           </div>
         </div>
 
-        <div className="absolute right-0 top-[2rem] w-full md:top-auto md:w-[50vw] h-[286px] md:h-[647px]">
-          <div className="relative w-full h-full">
-            <Image
-              src="/hero.jpeg"
-              layout="fill"
-              objectFit="cover"
-              alt="Hero Image"
-            />
+        <div className='absolute right-0 top-[2rem] w-full md:top-auto md:w-[50vw] h-[286px] md:h-[647px]'>
+          <div className='relative w-full h-full'>
+            <Image src='/hero.jpeg' layout='fill' objectFit='cover' alt='Hero Image' />
+          </div>
+        </div>
+      </div>
+      <div className='bg-background pb-20'>
+        <div className='container overflow-hidden'>
+          <div className='flex space-x-6 justify-between'>
+            <Card img={'/image14.jpg'} title={'ITZY 2ND WORLD TOUR'} />
+            <Card img={'/image15.jpg'} title={'滅火器 Fire EX.《一生到底 One Life， One Shot》'} />
+            <Card img={'/image16.jpg'} title={'JP Saxe：A Grey Area World Tour'} />
           </div>
         </div>
       </div>
     </Layout>
-  );
+  )
 }
